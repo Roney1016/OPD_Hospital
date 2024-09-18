24391970bf0b3034fe6c40cd16bd4aa73701242f
manish kumar ravi <manishkumarravi1016@gmail.com>
2024-09-18 09:55:38 +0530

    e

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
-document.querySelector("#register-btn").addEventListener("click", () => {
+document.querySelector("#register-btn").addEventListener("click", (e) => {
+    e.preventDefault();
     console.log("print")
     window.location.href = `form.html`
 })
